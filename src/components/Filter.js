c9e3cf8b3036b5464c5a3dbf6174f324c9c4ccd0
jacklyne <jacklyne.owuor@student.moringaschool>
2025-10-15 11:21:14 +0300

    added fetch-crud-code-along

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,21 @@
+
 import React from "react";
 
 function Filter({ category, onCategoryChange }) {
   return (
     <div className="Filter">
+      <label>Filter by Category:</label>
       <select
         name="filter"
         value={category}
         onChange={(e) => onCategoryChange(e.target.value)}
       >
-        <option value="All">Filter by category</option>
+        <option value="All">All</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+        <option value="Meat">Meat</option>
+        <option value="Bakery">Bakery</option>
+        <option value="Misc">Misc</option>
       </select>
     </div>
   );
